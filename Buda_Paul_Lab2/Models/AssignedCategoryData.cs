af1d0d45a4c0ca011ea1769450aeb1ab2c772bf5
Paul Buda <paul.bg94@gmail.com>
2025-11-16 21:24:44 +0200

    11-16: Add category checkbox on book edit

---
+++ Buda_Paul_Lab2/Models/AssignedCategoryData.cs
@@ -0,0 +1,12 @@
+﻿namespace Buda_Paul_Lab2.Models
+{
+    public class AssignedCategoryData
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Assigned
+        {
+            get; set;
+        }
+    }
+}
