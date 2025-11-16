19661af38a2adb5e2f86c051cc76d7de79fa92e8
Paul Buda <paul.bg94@gmail.com>
2025-11-16 20:09:33 +0200

    1-6: add Category add BookCategory add migration bookCategory

--- Buda_Paul_Lab2/Models/Book.cs
+++ Buda_Paul_Lab2/Models/Book.cs
@@ -22,5 +22,7 @@ namespace Buda_Paul_Lab2.Models
 
         public int? AuthorID { get; set; }
         public Author? Author { get; set; } //navigation property
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
