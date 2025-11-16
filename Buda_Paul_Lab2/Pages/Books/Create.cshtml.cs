27c9f60f925a15e5ad2e52ca498bc941b3bba0a8
Paul Buda <paul.bg94@gmail.com>
2025-11-16 21:43:31 +0200

    17-22: Add category checkbox to book create page. Show category on book list from index

--- Buda_Paul_Lab2/Pages/Books/Create.cshtml.cs
+++ Buda_Paul_Lab2/Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Buda_Paul_Lab2.Models;
 
 namespace Buda_Paul_Lab2.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Buda_Paul_Lab2.Data.Buda_Paul_Lab2Context _context;
 
@@ -23,6 +23,9 @@ namespace Buda_Paul_Lab2.Pages.Books
         {
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
             ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "FullName");
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
             return Page();
         }
 
@@ -30,17 +33,26 @@ namespace Buda_Paul_Lab2.Pages.Books
         public Book Book { get; set; } = default!;
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
-            if (!ModelState.IsValid)
+            var newBook = new Book();
+            if (selectedCategories != null)
             {
-                return Page();
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
             }
-
+            Book.BookCategories = newBook.BookCategories;
             _context.Book.Add(Book);
             await _context.SaveChangesAsync();
-
             return RedirectToPage("./Index");
         }
+
     }
 }
--- Buda_Paul_Lab2/Pages/Books/Index.cshtml.cs
+++ Buda_Paul_Lab2/Pages/Books/Index.cshtml.cs
@@ -19,14 +19,26 @@ namespace Buda_Paul_Lab2.Pages.Books
             _context = context;
         }
 
-        public IList<Book> Book { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public IList<Book> Book { get; set; }
+        public BookData BookD { get; set; }
+        public int BookID { get; set; }
+        public int CategoryID
         {
-            Book = await _context.Book
-                .Include(b => b.Publisher)
-                .Include(b => b.Author)
-                .ToListAsync();
+            get; set;
+        }
+
+        public async Task OnGetAsync(int? id, int? categoryID)
+        {
+            BookD = new BookData();
+
+            BookD.Books = await _context.Book.Include(b => b.Publisher).Include(b => b.BookCategories).ThenInclude(b => b.Category).AsNoTracking().OrderBy(b => b.Title).ToListAsync();
+            if (id != null)
+            {
+                BookID = id.Value;
+                Book book = BookD.Books
+                .Where(i => i.ID == id.Value).Single();
+                BookD.Categories = book.BookCategories.Select(s => s.Category);
+            }
         }
     }
 }
