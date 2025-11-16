a391919050099b38b926807ed8b513e362db00be
Paul Buda <paul.bg94@gmail.com>
2025-11-16 21:05:53 +0200

    7-10 add Categirues to Pages refactor nav-bar

---
+++ Buda_Paul_Lab2/Pages/Categories/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Buda_Paul_Lab2.Data;
+using Buda_Paul_Lab2.Models;
+
+namespace Buda_Paul_Lab2.Pages.Categories
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Buda_Paul_Lab2.Data.Buda_Paul_Lab2Context _context;
+
+        public DetailsModel(Buda_Paul_Lab2.Data.Buda_Paul_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (category is not null)
+            {
+                Category = category;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
