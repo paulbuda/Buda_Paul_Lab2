718af0c27047a0507514efa03bc83ba0ceeab88a
Paul Buda <paul.bg94@gmail.com>
2025-11-16 16:59:42 +0200

    Add project files.

---
+++ Buda_Paul_Lab2/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Buda_Paul_Lab2.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
