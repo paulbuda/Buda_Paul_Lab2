d8bba5ef0a00c598e19b2a5187db886bf36f4b23
Paul Buda <paul.bg94@gmail.com>
2025-12-15 20:00:54 +0200

    laborator 4: 2-3 publisherindex view model

---
+++ Buda_Paul_Lab2/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+using Buda_Paul_Lab2.Models;
+
+namespace Buda_Paul_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
