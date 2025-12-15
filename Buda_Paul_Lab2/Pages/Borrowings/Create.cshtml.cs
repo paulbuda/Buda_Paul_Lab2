ffa178ab43f13608b4ee2223e2f72ef170f8dd5e
Paul Buda <paul.bg94@gmail.com>
2025-12-15 21:25:01 +0200

    laboratorul-5: 6-9

---
+++ Buda_Paul_Lab2/Pages/Borrowings/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Buda_Paul_Lab2.Data;
+using Buda_Paul_Lab2.Models;
+
+namespace Buda_Paul_Lab2.Pages.Borrowings
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Buda_Paul_Lab2.Data.Buda_Paul_Lab2Context _context;
+
+        public CreateModel(Buda_Paul_Lab2.Data.Buda_Paul_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["BookID"] = new SelectList(_context.Book, "ID", "ID");
+        ViewData["MemberID"] = new SelectList(_context.Member, "ID", "ID");
+            return Page();
+        }
+
+        [BindProperty]
+        public Borrowing Borrowing { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Borrowing.Add(Borrowing);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
