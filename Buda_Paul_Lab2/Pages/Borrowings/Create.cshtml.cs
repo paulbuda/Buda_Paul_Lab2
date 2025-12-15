a693d6385457f02a4c09ec537720c422b0768819
Paul Buda <paul.bg94@gmail.com>
2025-12-15 21:30:16 +0200

    laboratorul 5: 10-12

--- Buda_Paul_Lab2/Pages/Borrowings/Create.cshtml.cs
+++ Buda_Paul_Lab2/Pages/Borrowings/Create.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Buda_Paul_Lab2.Data;
 using Buda_Paul_Lab2.Models;
 
@@ -21,8 +22,16 @@ namespace Buda_Paul_Lab2.Pages.Borrowings
 
         public IActionResult OnGet()
         {
-        ViewData["BookID"] = new SelectList(_context.Book, "ID", "ID");
-        ViewData["MemberID"] = new SelectList(_context.Member, "ID", "ID");
+            var bookList = _context.Book
+                .Include(b => b.Author)
+                .Select(x => new
+                {
+                    x.ID,
+                    BookFullName = x.Title + " - " + x.Author.LastName + " " + x.Author.FirstName
+                });
+
+            ViewData["BookID"] = new SelectList(bookList, "ID", "BookFullName");
+            ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName");
             return Page();
         }
 
--- Buda_Paul_Lab2/Pages/Borrowings/Index.cshtml.cs
+++ Buda_Paul_Lab2/Pages/Borrowings/Index.cshtml.cs
@@ -25,6 +25,7 @@ namespace Buda_Paul_Lab2.Pages.Borrowings
         {
             Borrowing = await _context.Borrowing
                 .Include(b => b.Book)
+                .ThenInclude(b => b.Author)
                 .Include(b => b.Member).ToListAsync();
         }
     }
