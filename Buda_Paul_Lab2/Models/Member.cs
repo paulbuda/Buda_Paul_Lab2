d69a71a9159a8565fc8d2e5c892946526e46926d
Paul Buda <paul.bg94@gmail.com>
2025-12-16 17:49:23 +0200

    laborator 6: Sarcina laborator 3

--- Buda_Paul_Lab2/Models/Member.cs
+++ Buda_Paul_Lab2/Models/Member.cs
@@ -14,7 +14,7 @@ namespace Buda_Paul_Lab2.Models
         [StringLength(70)]
         public string? Adress { get; set; }
         public string Email { get; set; }
-        [RegularExpression(@"^\(?([0-9]{4})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Telefonul trebuie sa fie de forma '0722-123-123' sau '0722.123.123' sau '0722 123 123'")]
+        [RegularExpression(@"^\(?0([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Telefonul trebuie sa fie de forma '0722-123-123' sau '0722.123.123' sau '0722 123 123'")]
         public string? Phone { get; set; }
         [Display(Name = "Full Name")]
         public string? FullName
