6d827f5cd3be0c725f1ef2086533248ac9f740ff
Paul Buda <paul.bg94@gmail.com>
2025-12-16 17:46:32 +0200

    laborator 6: Sarcina laborator 1

--- Buda_Paul_Lab2/Pages/Categories/Edit.cshtml.cs
+++ Buda_Paul_Lab2/Pages/Categories/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using Buda_Paul_Lab2.Models;
 
 namespace Buda_Paul_Lab2.Pages.Categories
 {
+    [Authorize(Roles = "Admin")]
     public class EditModel : PageModel
     {
         private readonly Buda_Paul_Lab2.Data.Buda_Paul_Lab2Context _context;
--- Buda_Paul_Lab2/Pages/Publishers/Create.cshtml.cs
+++ Buda_Paul_Lab2/Pages/Publishers/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +11,7 @@ using Buda_Paul_Lab2.Models;
 
 namespace Buda_Paul_Lab2.Pages.Publishers
 {
+    [Authorize(Roles = "Admin")]
     public class CreateModel : PageModel
     {
         private readonly Buda_Paul_Lab2.Data.Buda_Paul_Lab2Context _context;
