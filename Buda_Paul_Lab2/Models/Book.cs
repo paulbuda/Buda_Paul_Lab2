a694585b743f78434c14103838dccb660e97280e
Paul Buda <paul.bg94@gmail.com>
2025-12-16 17:43:55 +0200

    laboratorul 6: 12-16

--- Buda_Paul_Lab2/Models/Book.cs
+++ Buda_Paul_Lab2/Models/Book.cs
@@ -12,6 +12,7 @@ namespace Buda_Paul_Lab2.Models
         public string Title { get; set; }
 
         [Column(TypeName = "decimal(6, 2)")]
+        [Range(0.01, 500)]
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
